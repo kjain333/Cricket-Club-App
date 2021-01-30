117a37bf9ce948edd5453577b28f3d3e585a7950
Sachit-Kalia <bornon1999@gmail.com>
2021-01-30 14:58:49 +0530

    Changes done!! DP upload done!!

--- app/src/main/java/com/example/cricketclubapp/UserProfileActivity.java
+++ app/src/main/java/com/example/cricketclubapp/UserProfileActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,11 +19,16 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.ktx.Firebase;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserProfileActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private DatabaseReference databaseReference;
     TextView profileUserName, profileHostel, profileProgramme, profileSpeciality;
+    CircleImageView profilepic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,14 +36,45 @@ public class UserProfileActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         profileUserName = (TextView) findViewById(R.id.profileUserName);
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("user");
         profileHostel = (TextView) findViewById(R.id.profileHostel);
         profileProgramme = (TextView) findViewById(R.id.profileProgramme);
         profileSpeciality = (TextView) findViewById(R.id.profileSpeciality);
+        profilepic = findViewById(R.id.profilepic);
         Intent intent = getIntent();
         profileUserName.setText(intent.getStringExtra("name"));
         profileProgramme.setText(intent.getStringExtra("programme"));
         profileHostel.setText(intent.getStringExtra("hostel"));
         profileSpeciality.setText(intent.getStringExtra("speciality"));
+
+        profilepic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), EditProfile.class);
+                startActivity(intent);
+            }
+        });
+
+        getUserInfo();
+    }
+
+    private void getUserInfo() {
+        databaseReference.child(mAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists() && snapshot.getChildrenCount() > 0){
+                    if(snapshot.hasChild("image")){
+                        String image = snapshot.child("image").getValue().toString();
+                        Picasso.get().load(image).into(profilepic);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     public void logoutUser(View view){
