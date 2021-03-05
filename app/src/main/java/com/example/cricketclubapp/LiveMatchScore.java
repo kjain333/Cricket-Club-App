5b4f9beda5a4b52f36721b8209eccdfd854ea8aa
kjain333 <karanjain140701@gmail.com>
2021-03-05 23:45:08 +0530

    update 1

--- app/src/main/java/com/example/cricketclubapp/LiveMatchScore.java
+++ app/src/main/java/com/example/cricketclubapp/LiveMatchScore.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -12,11 +13,20 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class LiveMatchScore extends AppCompatActivity {
-
+    TextView tv1,tv2,tv3,tv4,tv5,tv6,tv7,tv8,tv9;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_live_match_score);
+        setContentView(R.layout.activity_live_scores);
+        tv1 = findViewById(R.id.teamsTV);
+        tv2 = findViewById(R.id.battingTeam);
+        tv3 = findViewById(R.id.runs);
+        tv4 = findViewById(R.id.overs);
+        tv5 = findViewById(R.id.target);
+        tv6 = findViewById(R.id.batsmanName1);
+        tv7 = findViewById(R.id.batsmanScore1);
+        tv8 = findViewById(R.id.batsmanName2);
+        tv9 = findViewById(R.id.batsmanScore2);
         FirebaseFirestore.getInstance().collection("matchScore").document("6Zi758UlT1ruNem97kZC").addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
@@ -37,7 +47,19 @@ public class LiveMatchScore extends AppCompatActivity {
                     batsman2 = value.get("secondBatsmanName").toString();
                     batsman2score = value.get("secondBatsmanScore").toString();
                     target = value.get("target").toString();
-                    Toast.makeText(getBaseContext(),batsman1+target+batsman1score+batsman2+batsman2score+battingTeam+bowlingTeam+runs+overs+wickets,Toast.LENGTH_LONG).show();
+                    if(target=="")
+                        tv5.setText("1st innings");
+                    else
+                        tv5.setText("target: "+target);
+                    tv1.setText(battingTeam+" vs "+bowlingTeam);
+                    tv2.setText(battingTeam);
+                    tv3.setText(runs+"/"+wickets);
+                    tv4.setText(overs);
+                    tv6.setText(batsman1);
+                    tv7.setText(batsman1score);
+                    tv8.setText(batsman2);
+                    tv9.setText(batsman2score);
+                    //Toast.makeText(getBaseContext(),batsman1+target+batsman1score+batsman2+batsman2score+battingTeam+bowlingTeam+runs+overs+wickets,Toast.LENGTH_LONG).show();
                 }
             }
         });
